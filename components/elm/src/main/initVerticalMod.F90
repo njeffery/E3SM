13a5a69d748f91b7dc437b8e014cebc7bca0d646
Teklu K Tesfa <teklu.tesfa@pnnl.gov>
2021-06-07 16:07:54 -0700

    Resolving conflict

--- components/elm/src/main/initVerticalMod.F90
+++ components/elm/src/main/initVerticalMod.F90
@@ -23,10 +23,10 @@ module initVerticalMod
   use landunit_varcon, only : istdlak, istice_mec
   use fileutils      , only : getfil
   use LandunitType   , only : lun_pp                
-  use ColumnType     , only : col_pp           
+  use ColumnType     , only : col_pp          
+  use ncdio_pio
   use topounit_varcon, only : max_topounits
   use GridcellType     , only : grc_pp
-  use ncdio_pio
   !
   ! !PUBLIC TYPES:
   implicit none
