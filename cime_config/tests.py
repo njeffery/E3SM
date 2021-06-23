3043205ae64f10629061853d8e66141f265bd465
Walter Hannah <hannah6@llnl.gov>
2021-06-23 16:12:34 -0700

    Remove task/thread spcification for MMF tests
    	modified:   cime_config/tests.py

--- cime_config/tests.py
+++ cime_config/tests.py
@@ -133,7 +133,7 @@ _TESTS = {
             "SMS.T62_oQU120_ais20.MPAS_LISIO_TEST",
             "SMS.f09_g16_a.IGELM_MLI",
             "SMS_P12x2.ne4_oQU240.A_WCYCL1850.allactive-mach_mods",
-            "ERS_Ln9_P96x1.ne4pg2_ne4pg2.F-MMF1.eam-mmf_crmout",
+            "ERS_Ln9.ne4pg2_ne4pg2.F-MMF1.eam-mmf_crmout",
             "SMS_B.ne4_ne4.F-EAM-AQP1.eam-hommexx",
             )
         },
@@ -143,6 +143,16 @@ _TESTS = {
         "tests"   : (
             "HOMME_P24.f19_g16_rx1.A",
             "HOMMEBFB_P24.f19_g16_rx1.A",
+            )
+        },
+
+    "mmf_tmp" : {
+        "time"    : "0:45:00",
+        "tests"   : (
+            "ERS_Ln9.ne4pg2_ne4pg2.F-MMF1.eam-mmf_crmout",
+            "ERP_Ln9.ne4pg2_ne4pg2.F-MMFXX.eam-mmf_fixed_subcycle",
+            "ERS_Ln9.ne4pg2_ne4pg2.F-MMFXX.eam-mmf_use_VT",
+            "ERS_Ln9.ne4pg2_ne4pg2.F-MMFXX.eam-mmf_use_ESMT",
             )
         },
 
@@ -165,11 +175,11 @@ _TESTS = {
             "SMS_Ld2.ne30_oECv3.BGCEXP_BCRC_CNPRDCTC_1850.elm-bgcexp",
             "SMS_D_Ld1.T62_oEC60to30v3.DTESTM",
             "SMS_D_Ld1.ne30_r05_oECv3.A_WCYCL1850",
-            "ERP_Ln9_P96x1.ne4pg2_ne4pg2.F-MMFXX.eam-mmf_fixed_subcycle",
-            "ERS_Ln9_P96x1.ne4pg2_ne4pg2.F-MMFXX.eam-mmf_use_VT",
-            "ERS_Ln9_P96x1.ne4pg2_ne4pg2.F-MMFXX.eam-mmf_use_ESMT",
-            "ERP_Ln9_P96x1.ne4pg2_ne4pg2.F-MMFOMP.eam-mmf_fixed_subcycle",
-            "ERS_Ln9_P96x1.ne4pg2_ne4pg2.F-MMF1-RCEMIP",
+            "ERP_Ln9.ne4pg2_ne4pg2.F-MMFXX.eam-mmf_fixed_subcycle",
+            "ERS_Ln9.ne4pg2_ne4pg2.F-MMFXX.eam-mmf_use_VT",
+            "ERS_Ln9.ne4pg2_ne4pg2.F-MMFXX.eam-mmf_use_ESMT",
+            "ERP_Ln9.ne4pg2_ne4pg2.F-MMFOMP.eam-mmf_fixed_subcycle",
+            "ERS_Ln9.ne4pg2_ne4pg2.F-MMF1-RCEMIP",
             )
         },
 
@@ -205,12 +215,12 @@ _TESTS = {
         "time" : "02:00:00",
         "tests" : (
             # MMF tests
-            "SMS_D_Ln3_P96x1.ne4pg2_ne4pg2.F-MMF1",
-            "SMS_Ln3_P96x1.ne4pg2_ne4pg2.F-MMFXX-AQP1",
-            "SMS_Ln3_P96x1.ne4pg2_ne4pg2.F-MMFXX-RCEMIP",
-            "ERS_Ln9_P96x1.ne4pg2_ne4pg2.F-MMF1.eam-mmf_crmout",
-            "ERS_Ln9_P96x1.ne4pg2_ne4pg2.F-MMFXX.eam-mmf_crmout",
-            "SMS_Ln9_P96x1.ne4pg2_ne4pg2.F-MMF2-ECPP",
+            "SMS_D_Ln3.ne4pg2_ne4pg2.F-MMF1",
+            "SMS_Ln3.ne4pg2_ne4pg2.F-MMFXX-AQP1",
+            "SMS_Ln3.ne4pg2_ne4pg2.F-MMFXX-RCEMIP",
+            "ERS_Ln9.ne4pg2_ne4pg2.F-MMF1.eam-mmf_crmout",
+            "ERS_Ln9.ne4pg2_ne4pg2.F-MMFXX.eam-mmf_crmout",
+            "SMS_Ln9.ne4pg2_ne4pg2.F-MMF2-ECPP",
             # non-MMF tests with RRTMGP
             "ERP_Ln9.ne4pg2_ne4pg2.FC5AV1C-L.eam-rrtmgp",
             )
